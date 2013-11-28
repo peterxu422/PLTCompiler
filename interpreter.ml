948d593623b401ebc4a09054785dafcc21a2a76c
Peter Xu <peterxu422@gmail.com>
2013-11-28 18:03:42 -0500

    implemented variable assignments

--- interpreter.ml
+++ interpreter.ml
@@ -5,6 +5,8 @@ module NameMap = Map.Make(struct
 	let compare x y = Pervasives.compare x y
 end)
 
+exception ReturnException of expr * expr NameMap.t
+
 let initType t = 
   match t with
     "int" -> Int(0)
@@ -22,25 +24,46 @@ let run (vars, funcs) =
 
 		(* evals expressions and updates globals *)
 		let rec eval env = function
-
-			| Int(i) -> Int(i), env
+			 Int(i) -> Int(i), env
+			| Double(d) -> Double(d), env
+			| Boolean(b) -> Boolean(b), env
 			| Pitch(p) -> Pitch(p), env
+			| Sound(s) -> Sound(s), env
 			(* THIS ISNT RIGHT *)
-			| Id(var) -> Id(var), env
-
-(* 			| Assign(var, e) ->
+			| Id(var) -> 
+			let locals, globals = env in
+			if NameMap.mem var locals then
+				(NameMap.find var locals), env
+			else if NameMap.mem var globals then
+				(NameMap.find var globals), env
+			else raise (Failure ("undeclared identifier " ^ var))
+ 			| Assign(var, e) ->
+			let v, (locals, globals) = eval env e in
+			(match var with
+				Id(name) ->
+				(* The local identifiers have already been added to ST in the first pass. 
+				Checks if it is indeed in there.*)
+				if NameMap.mem name locals then	
+					v, (NameMap.add name v locals, globals) (* Updates the var in the ST to evaluated expression e *)
+				else if NameMap.mem name globals then
+					v, (locals, NameMap.add name v globals)
+				else raise (Failure ("undeclared identifier " ^ name))
+			| _ -> raise (Failure ("Can only assign variables or array indices")))
+			(*
 				let v1, env = eval env var in
-				let e1, (locals, globals) = eval env e in *)
-			
+				let e1, (locals, globals) = eval env e in 
+			*)
 			(* Arrays *)
 			| Array(e) -> Array(e), env
 			
 			(* our special print function, only supports ints right now *)
 			| Call("print", [e]) -> 
-				let v, _ = eval env e in
+				let v, env = eval env e in
 				
 				let rec print = function
 					Int(i) -> string_of_int i
+					| Double(d) -> string_of_float d
+					| Boolean(b) -> string_of_bool b
 					| Pitch(p) -> p
 					| Sound(s) -> s
 					| Array(a) -> "[" ^ build a ^ "]" and build = function
@@ -72,11 +95,13 @@ let run (vars, funcs) =
 					(fun (actuals, env) actual ->
 						print_endline (Ast.string_of_expr actual);
 					let v, env = eval env actual in v :: actuals, env)
-					([], env) actuals
+					([], env) (List.rev actuals)
 				in
 				let (locals, globals) = env in
-					let globals = call fdecl (List.rev actuals) []
+				try
+					let globals = call fdecl actuals globals
 					in Boolean(false), (locals, globals)
+				with ReturnException(v, globals) -> v, (locals, globals)
 			in
 
 			(* executes statements, calls evals on expressions *)
@@ -84,33 +109,30 @@ let run (vars, funcs) =
 				| Expr(e) -> let _, env = eval env e in env
 			in  
 
-			(* do locals yo *)
+			(* Enter the function: bind actual values to formal arguments *)
 			let locals = 
 				try List.fold_left2
-					(fun locals formal actual ->
-					NameMap.add formal.paramname actual locals)
-				NameMap.empty fdecl.formals actuals
+					(fun locals formal actual -> NameMap.add formal.paramname actual locals) NameMap.empty fdecl.formals actuals
 				with Invalid_argument(_) ->
 					raise (Failure ("wrong number of arguments to: " ^ fdecl.fname))
 			in 
 			let locals = List.fold_left (* init locals to 0 *)
-				(fun locals local -> NameMap.add local.varname (initType local.vartype) locals)
-				locals fdecl.locals
+				(fun locals local -> NameMap.add local.varname (initType local.vartype) locals) locals fdecl.locals
 			in
-
-
 			(* this should actually take in env eventually. I think
 			   that the fold left will accumulate (locals, globals),
 			   which will be returned by stuff above*)
 	    	snd (List.fold_left exec (locals, globals) fdecl.body)
 
-		in
+		in let globals = List.fold_left
+			(fun globals vdecl -> NameMap.add vdecl.varname (initType vdecl.vartype) globals) NameMap.empty vars
+		in try
 		(* needs globals i think *)
-	    call (NameMap.find "main" func_decls) [] []
+			call (NameMap.find "main" func_decls) [] globals
+		with Not_found -> raise (Failure("did not find the main() function"))
 
 let _ = 
 	let lexbuf = Lexing.from_channel stdin in 
-	(*let stmt = Parser.stmt Scanner.token lexbuf in*)
 	let program = Parser.program Scanner.token lexbuf in
 	run program
 		(*print_endline (Ast.string_of_program program)*)
