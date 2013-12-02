fec378337df11198a6da70ccda9758f29d378bb5
Louis Croce <ljc2154@columbia.edu>
2013-12-01 21:07:10 -0500

    I undid these changes

--- interpreter.ml
+++ interpreter.ml
@@ -5,9 +5,32 @@ module NameMap = Map.Make(struct
 	let compare x y = Pervasives.compare x y
 end)
 
+let getType v = 
+	match v with
+		Int(v) -> "int"
+		| Double(v) -> "double"
+		| Boolean(v) -> "bool"
+
+let getInt v = 
+	match v with
+		Int(v) -> v
+		| _ -> 0
+
+let getDouble v =
+	match v with
+		Double(v) -> v
+		| _ -> 0.0
+
+let getBoolean v =
+	match v with
+		Boolean(v) -> v
+		| _ -> false
+
 let initType t = 
   match t with
     "int" -> Int(0)
+    | "double" -> Double(0.0)
+    | "bool" -> Boolean(false)
     | _ -> Boolean(false)
 
 let run (vars, funcs) =
@@ -23,14 +46,103 @@ let run (vars, funcs) =
 		(* evals expressions and updates globals *)
 		let rec eval env = function
 
-			| Int(i) -> Int(i), env
-
+			| Int(i) -> Int i, env
+			| Double(i) -> Double i, env
+			| Boolean(i) -> Boolean i, env
 			(* THIS ISNT RIGHT *)
 			| Id(var) -> Id(var), env
 
 (* 			| Assign(var, e) ->
 				let v1, env = eval env var in
 				let e1, (locals, globals) = eval env e in *)
+
+
+			| Binop(e1, op, e2) ->
+				let v1, env = eval env e1 in
+				let v2, env = eval env e2 in
+				let v1Type = getType v1 in
+				let v2Type = getType v2 in
+
+				if v1Type = v2Type then
+					(match op with
+						Add -> 
+							if v1Type = "int" then
+								Int (getInt v1 + getInt v2)
+							else if v1Type = "double" then
+								Double (getDouble v1 +. getDouble v2)
+							else raise (Failure (v1Type ^ " has no + operator"))
+						| Sub -> 
+							if v1Type = "int" then
+								Int (getInt v1 - getInt v2)
+							else if v1Type = "double" then
+								Double (getDouble v1 -. getDouble v2)
+							else raise (Failure (v1Type ^ " has no - operator"))
+						| Mult ->
+							if v1Type = "int" then
+								Int (getInt v1 * getInt v2)
+							else if v1Type = "double" then
+								Double (getDouble v1 *. getDouble v2)
+							else raise (Failure (v1Type ^ " has no * operator"))
+						| Div ->
+							if v1Type = "int" then
+								Int (getInt v1 / getInt v2)
+							else if v1Type = "double" then
+								Double (getDouble v1 /. getDouble v2)
+							else raise (Failure (v1Type ^ " has no / operator"))
+						| Mod ->
+							if v1Type = "int" then
+								Int (getInt v1 mod getInt v2)
+							else raise (Failure (v1Type ^ " has no % operator"))
+						| Or -> 
+							if v1Type = "bool" then
+								Boolean (getBoolean v1 || getBoolean v2)
+							else raise (Failure (v1Type ^ " has no || operator"))
+						| And -> 
+							if v1Type = "bool" then
+								Boolean (getBoolean v1 && getBoolean v2)
+							else raise (Failure (v1Type ^ " has no && operator"))
+						| Eq -> 
+							if v1Type = "int" then
+								Boolean (getInt v1 = getInt v2)
+							else if v1Type = "double" then
+								Boolean (getDouble v1 = getDouble v2)
+							else if v1Type = "bool" then
+								Boolean (getBoolean v1 = getBoolean v2)
+							else raise (Failure (v1Type ^ " has no == operator"))
+						| Neq -> 
+							if v1Type = "int" then
+								Boolean (getInt v1 <> getInt v2)
+							else if v1Type = "double" then
+								Boolean (getDouble v1 <> getDouble v2)
+							else if v1Type = "bool" then
+								Boolean (getBoolean v1 <> getBoolean v2)
+							else raise (Failure (v1Type ^ " has no != operator"))
+						| Lt ->
+							if v1Type = "int" then
+								Boolean (getInt v1 < getInt v2)
+							else if v1Type = "double" then
+								Boolean (getDouble v1 < getDouble v2)
+							else raise (Failure (v1Type ^ " has no < operator"))
+						| Gt ->
+							if v1Type = "int" then
+								Boolean (getInt v1 > getInt v2)
+							else if v1Type = "double" then
+								Boolean (getDouble v1 > getDouble v2)
+							else raise (Failure (v1Type ^ " has no > operator"))
+						| Leq ->
+							if v1Type = "int" then
+								Boolean (getInt v1 <= getInt v2)
+							else if v1Type = "double" then
+								Boolean (getDouble v1 <= getDouble v2)
+							else raise (Failure (v1Type ^ " has no <= operator"))
+						| Geq ->
+							if v1Type = "int" then
+								Boolean (getInt v1 >= getInt v2)
+							else if v1Type = "double" then
+								Boolean (getDouble v1 >= getDouble v2)
+							else raise (Failure (v1Type ^ " has no * operator"))
+						), env
+				else raise (Failure ("Types " ^ v1Type ^ " and " ^ v2Type ^ " do not match"))
 
 
 			(* our special print function, only supports ints right now *)
@@ -97,6 +209,6 @@ let _ =
 	let lexbuf = Lexing.from_channel stdin in 
 	(*let stmt = Parser.stmt Scanner.token lexbuf in*)
 	let program = Parser.program Scanner.token lexbuf in
-	(*run program*)
-		print_endline (Ast.string_of_program program)
+	run program
+	(*	print_endline (Ast.string_of_program program) *)
 	
