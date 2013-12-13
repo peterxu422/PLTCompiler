8685ea5cc67739e70ef63a419bd0adef5c790e2f
Ben Nappier <ben2113@columbia.edu>
2013-12-13 00:14:26 -0500

    Mixdown can now take a in the specified track_num eg. mixdown(a, 2) appends to track two. mixdown can be called many times

--- BytecodeTranslator.java
+++ BytecodeTranslator.java
@@ -18,56 +18,70 @@ public class BytecodeTranslator {
 		Pattern p = new Pattern();
 		String currentLine;
 		String byteCode = "";
-		String[] tracks;
+		String[] tracks = new String[16];
+		String[] mixDownWrites;
+
+		String test = "";
 
 		try {
 
 			br = new BufferedReader(new FileReader(fileName));
 			while ((currentLine = br.readLine()) != null) {
-				byteCode += currentLine.substring(1, currentLine.length()-1) + "\n";	
+				byteCode += currentLine.charAt(0) + currentLine.substring(2, currentLine.length()-1) + "\n";	
 			}
 
-			tracks = byteCode.split("\n");
+			mixDownWrites = byteCode.split("\n");
 
-			for (int i=0;i<1/*tracks.length*/;i++){
-				String track="";
-				String chord="";
-				//System.out.println(tracks[i]);
-				//System.out.println("break");
-				track += "V" + i + " ";
-
-				String[] sounds = tracks[i].split("\\[");
-				
-				for (int j=1;j<sounds.length;j++){
-					String[] chordsDurAmp = sounds[j].split(":");
-
-					String amp = "";
-					if(chordsDurAmp[2].endsWith(",")){
-						amp = chordsDurAmp[2].replace(",", "");
-					}else{
-						amp = chordsDurAmp[2];
-					}
-
-					// changed this so that it supports doubles inside of the sound too
-					double durr;
-					if (chordsDurAmp[1].indexOf(".") == -1){
-						System.out.println("no /");
-						String[] rat = chordsDurAmp[1].split("/");
-        				durr = Double.parseDouble(rat[0]) / Double.parseDouble(rat[1]);
-					} else {
-        				durr = Double.parseDouble(chordsDurAmp[1]);	
-					}
-					
-
-					String[] chords = chordsDurAmp[0].split(" ");
-					for(int l=0;l<chords.length-1;l++){
-						chord += chords[l].substring(0, chords[l].length()-1) + "/" + durr + "a" + amp + "+";
-					}
-						chord += chords[chords.length-1].substring(0, chords[chords.length-1].length()-1) + "/" + durr + "a" + amp + " ";
+			for(int i = 0; i<mixDownWrites.length; i++){
+				int trackNum = Integer.parseInt(String.valueOf(mixDownWrites[i].charAt(0)));
+				if(tracks[trackNum] == null){
+					tracks[trackNum] = mixDownWrites[i].substring(1);
+				} else{
+					tracks[trackNum] += mixDownWrites[i].substring(1);
 				}
-				chord = chord.substring(0, chord.length()-1);
-				track += chord;
-				p.add(track);
+			}
+
+			for (int i=0;i<tracks.length;i++){
+				if(tracks[i] != null){
+					String track="";
+					String chord="";
+					if(i > 0){
+						track += " " + "V" + i + " ";
+					} else {
+						track += "V" + i + " ";
+					}
+
+					String[] sounds = tracks[i].split("\\[");
+					
+					for (int j=1;j<sounds.length;j++){
+						String[] chordsDurAmp = sounds[j].split(":");
+
+						String amp = "";
+						if(chordsDurAmp[2].endsWith(",")){
+							amp = chordsDurAmp[2].replace(",", "");
+						}else{
+							amp = chordsDurAmp[2];
+						}
+
+						double durr;
+						if (chordsDurAmp[1].indexOf(".") == -1){
+							String[] rat = chordsDurAmp[1].split("/");
+	        				durr = Double.parseDouble(rat[0]) / Double.parseDouble(rat[1]);
+						} else {
+	        				durr = Double.parseDouble(chordsDurAmp[1]);	
+						}
+
+						String[] chords = chordsDurAmp[0].split(" ");
+						for(int l=0;l<chords.length-1;l++){
+							chord += chords[l].substring(0, chords[l].length()-1) + "/" + durr + "a" + amp + "+";
+						}
+							chord += chords[chords.length-1].substring(0, chords[chords.length-1].length()-1) + "/" + durr + "a" + amp + " ";
+					}
+					chord = chord.substring(0, chord.length()-1);
+					track += chord;
+					test += track;
+					p.add(track);
+				}
 			}
 
 			player.saveMidi(p, new File("music-file.mid")); 
